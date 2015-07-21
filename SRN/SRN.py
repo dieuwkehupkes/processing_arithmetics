5d6b99e32f1fa2d164b4bcb32ac486cddffec0e8
dieuwkehupkes <dieuwkehupkes@gmail.com>
2015-07-21 17:23:39 +0200

    drafty buggy version of training SRN

--- SRN/SRN.py
+++ SRN/SRN.py
@@ -1,8 +1,13 @@
 # A class representing a simple recurrent neural network
+# The setup is not very re-usable, if I want to do more things
+# with this later I'll take the code and make sure the training algorithm
+# and these things are a little more extendabel
 
 import numpy as np
 import copy
-from softmax import softmax
+import itertools
+from softmax import all
+from sigmoid import all
 
 
 class SimpleRecurrentNetwork():
@@ -57,7 +62,10 @@ class SimpleRecurrentNetwork():
 
     def update(self, input=None):
         """
-        Update all layers of the network
+        Update all layers of the network until input
+        has reached the end (i.e., the output and hidden layers
+        are updated multiple times within one "update")
+        @TODO: check if .dot is indeed the way to multiply
         """
         # set values inputLayer
         if input:
@@ -65,30 +73,70 @@ class SimpleRecurrentNetwork():
         else:
             self.I = np.zeros(self.I.size())
 
-        # make copy values hidden layer
-        hiddenCopy = copy.deepcopy(self.H)
-
         # set values hiddenLayer
-        self.H = np.tanh(self.I.dot(self.Wih) + self.C.dot(self.Wch))
+        self.H = np.tanh(self.Wih.dot(self.I) + self.Wch.dot(self.C))
 
         # set values outputLayer
-        self.O = softmax(hiddenCopy.dot(self.Who))
+        self.O = softmax(self.Who.dot(self.H))
 
-        # set values contextLayer
-        self.contextLayer = hiddenCopy
+        # set values contextLayer to hidden layer
+        self.C = self.H
 
-    def train(self, input_output, rounds=1):
+    def train(self, sequences, learning_rate = 0.1, rounds=1, depth=1):
         """
         Train the network with backpropagation to always
         predict the next vector in the inputSequence
         :param input_output: an array with tuples of input-output pairs
         """
+
         for round in xrange(rounds):
             self.reset()
-            self.I = input_output[0]
+
+            # create new training sequence by shuffling and then unpacking
+            training_sequence = list(itertools.chain(np.random.permutation(sequences)))
+            
+            self.I = training_sequence[0]
             self.update()
-            for pair in xrange(1, input_output.size()):
-                raise NotImplementedError
+
+            # create arrays to store previous states
+            prev_hidden = np.zeros((depth, self.H.size))
+            index_hidden = 0
+
+            # loop over training examples
+            for index in xrange(len(training_sequence)-1):
+                    training_example = training_sequence[index]
+
+                    self.I = training_example
+                    self.update()
+                    prev_hidden[index_hidden] = self.H              # store hidden state
+
+                    # update weights Who
+                    output_error = training_sequence[index+1] - training_example    # compute output error
+                    jacobian = jacobian_softmax(self.O)                             # compute jacobian matrix with partial derivatives
+                    update_Who = learning_rate * jacobian * self.H * output_error     # compute update (check even of dit goed gaat met de assen e.d.)
+
+                    # propagate error back to H
+                    hidden_error = self.O.dot(jacobian)                             # hier gaat ongetwijfeld ook iets mis, check dit even
+
+                    # set working timelag
+                    time_lag = 0
+                    update_Wch = np.zeros(self.Wch.shape)
+
+                    while time_lag <= depth and index >= time_lag:
+
+                        # update Wch
+                        jacobian = jacobian_sigmoid(self.H)
+                        update_Wch += learning_rate * jacobian * prev_hidden[index_hidden-time_lag] * hidden_error      # compute update for Wch
+
+                        # update Wih
+                        update_Wch += learning_rate * jacobian * training_sequence[index-time_lag] * [index_hidden-time_lag] * hidden_error      # compute update for Wch
+
+                        # propagate error one time step back
+                        hidden_error = prev_hidden[index].dot(jacobian)
+
+                        time_lag += 1
+
+            index_hidden = (index_hidden + 1) % depth       # switch index of storing hidden state
 
     def reset(self):
         """
