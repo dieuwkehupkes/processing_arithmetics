3b4e7963ee1330b8f227ef2104ad4ad71ebd5cc1
dieuwkehupkes <dieuwkehupkes@gmail.com>
2015-07-21 16:59:06 +0200

    added function to compute jacobian matrix with partial derivates

--- SRN/softmax.py
+++ SRN/softmax.py
@@ -5,3 +5,12 @@ def softmax(a):
     e = np.exp(a)
     marg = e.sum()
     return e/marg
+
+def jacobian_softmax(activation_vector):
+    size = activation_vector.shape[0]
+    jacobian = - np.outer(activation_vector, activation_vector)
+    jacobian[xrange(size), xrange(size)] += activation_vector 
+
+    return jacobian
+
+
