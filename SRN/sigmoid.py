d9f38d2e2cc92c42c272ec951771c389899a947b
dieuwkehupkes <dieuwkehupkes@gmail.com>
2015-07-23 16:05:42 +0200

    removed bug from output

--- SRN/sigmoid.py
+++ SRN/sigmoid.py
@@ -7,5 +7,5 @@ def sigmoid(a):
 def jacobian_sigmoid(activation_vector):
     size = activation_vector.shape[0]
     jacobian = np.zeros((size, size))
-    jacobian[xrange(size), xrange(size)] += activation_vector*activation_vector
+    jacobian[xrange(size), xrange(size)] += activation_vector*(1 - activation_vector)
     return jacobian
