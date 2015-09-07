8ab24338bbdf357dc72f3e138a3d5282cf99b116
dieuwkehupkes <dieuwkehupkes@gmail.com>
2015-09-07 16:16:07 +0200

    add iteration function to train

--- SRN/SRN_Theano.py
+++ SRN/SRN_Theano.py
@@ -50,6 +50,11 @@ class SimpleRecurrentNetwork():
         - How the error of a sequence of predictions can be computed from
           the input sequence
         - How to compute the gradients w.r.t the different weights
+
+
+        NB: I somehow feel like this should not all be in the same class,
+        but I am not really sure how to do it differently, because I
+        also don't want to recreate expressions more often than necessary
         """
 
         # function describing how the hidden layer can be computed from the input
@@ -71,20 +76,57 @@ class SimpleRecurrentNetwork():
         # gradients
         gradients = T.grad(error, self.params)
 
-        # updates @TODO is the sign of the update correct?
+        # updates
         new_params = self.params - gradients*learning_rate
 
         # inputs
-        # @TODO this should be somewhere else I think
+        # @TODO should I put this somewhere else?
+        # TODO klopt dit??? of moet ik echte waardes meegeven
         givens = {
-                input_sequence = data
-                h_t = h_value           # @TODO give expression for h_t, potentially have a value, initialise h
+                input_data:  input_sequence,
+                h_t:        self.hidden_t
         }
 
         self.update_function = theano.function(input_data, error, updates=new_params, givens=givens)        # @TODO klopt updates=+, 
 
+        return
+
+    def train(self, input_sequences, no_iterations, some_other_params):
+        """
+        Train the network to store input_sequences
+        :param input_sequences  
+        :param no_iterations    
+        """
+        #TODO write function description
+        for iteration in xrange(0, no_iterations):
+            iteration(input_sequences)
 
         return
+
+    def iteration(self, input_sequences):
+        """
+        Slice data in minibatches and perform one
+        training iteration.
+        :param input_sequences: The sequences we want to
+                                store in the network
+        """
+        batches = self.make_batches(input_sequences)
+
+        # loop over minibatches, update parameters
+        for batch in batches:
+            self.update_function(batch)
+
+        return
+
+    def make_batches(self, input_sequences):
+        """
+        Make batches from input sequence. 
+        Currently this doesn't do anything but return the
+        input sequence (for testing phase) but later this
+        should start doing some more things.
+        """
+        # TODO Make that this method actually does something
+        return input_sequences
 
     def prediction(self):
 
@@ -94,14 +136,4 @@ class SimpleRecurrentNetwork():
         # possibly desirable is, the symbolic expression for the prediction
         # for the prediction would change (as well as the output activation, btw)
         self.prediction = T.argmax(self.output, axis=1)
-
-
-
-"""
-Otto:
-volgens mij moeten we nu eerst voorspelling uitrekenen, dan je error berekenen, dan gradients definieren als afgeleide van error wrt je parameters, dan de learning update definieren, en dan de gegeven (givens) definieren en functie aanmaken:) dus eerst prediction uitrekenen
-en dan  learning update definieren, en dan de gegeven (givens) definieren
-en functie aanmaken:)
-"""
-
 
