9d99173a4048fd9c40e52edb49c90f56d168b0ad
dieuwkehupkes <dieuwkehupkes@gmail.com>
2015-09-11 18:01:46 +0200

    add forward_pass, matrix multiplication goes wrong atm

--- SRN/test_srn_theano.py
+++ SRN/test_srn_theano.py
@@ -2,7 +2,8 @@ from SRN_Theano import SRN
 import numpy as np
 
 network = SRN(3, 3, 3, 0.2)
-network.generate_network_dynamics()
+network.generate_update_function()
+# network.generate_network_dynamics()
 
 
 # test and training sequences
@@ -13,12 +14,7 @@ training_sequence = np.array([[a, b, c, a, b, c, a, b, c, a], [b, a, c, b, a, c,
 test_sequence1 = np.array([a, b, c, a, b, c, a, b, c])
 test_sequence2 = np.array([b, a, c, b, a, c, b, a, c])
 
-print network.U.get_value(), '\n'
-print network.V.get_value(), '\n'
-print network.W.get_value(), '\n'
+print network.activations['hidden_t']
+print network.activations['output_t']
 
-network.train(training_sequence, 1)
-
-print network.U.get_value(), '\n'
-print network.V.get_value(), '\n'
-print network.W.get_value()
+network.forward_pass(a)
