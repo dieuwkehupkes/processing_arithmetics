0adfce8ba8259f70498a2c10cbc337054aa9065e
dieuwkehupkes <dieuwkehupkes@gmail.com>
2015-09-11 11:38:12 +0200

    small test file to test SRN implemented in theano

---
+++ SRN/test_srn_theano.py
@@ -0,0 +1,24 @@
+from SRN_Theano import SRN
+import numpy as np
+
+network = SRN(3, 3, 3, 0.2)
+network.generate_network_dynamics()
+
+
+# test and training sequences
+a = np.array([0,0,1])
+b = np.array([0,1,0])
+c = np.array([1,0,0])
+training_sequence = np.array([[a, b, c, a, b, c, a, b, c, a], [b, a, c, b, a, c, b, a, c]])
+test_sequence1 = np.array([a, b, c, a, b, c, a, b, c])
+test_sequence2 = np.array([b, a, c, b, a, c, b, a, c])
+
+print network.U.get_value(), '\n'
+print network.V.get_value(), '\n'
+print network.W.get_value(), '\n'
+
+network.train(training_sequence, 1)
+
+print network.U.get_value(), '\n'
+print network.V.get_value(), '\n'
+print network.W.get_value()
