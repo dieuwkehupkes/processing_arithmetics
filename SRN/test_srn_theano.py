57e744c85913b7eabbfcf43c7c503581f983e720
dieuwkehupkes <dieuwkehupkes@gmail.com>
2015-09-25 17:23:48 +0200

    typo

--- SRN/test_srn_theano.py
+++ SRN/test_srn_theano.py
@@ -16,20 +16,23 @@ training_sequence1 = np.array([[a, b, c, a, b, c, a, b, c], [c, a, b, c, a, b]])
 training_sequence2 = np.array([[a, b, c, a, b, c, a, b, c, a], [b, a, c, b, a, c, b, a, c]])
 test_sequence1 = np.array([a, b, c, a, b, c, a, b, c])
 test_sequence2 = np.array([b, a, c, b, a, c, b, a, c])
+test_sequence3 = np.array([b, b, b, b, b, b, b, b, b])
 
-rounds = np.arange(0, 1000, 1)
+rounds = np.arange(0, 200, 1)
 error_same = []
 error_diff = []
 error_rand = []
 
 for round in rounds:
-    network.train(training_sequence1, 1)
+    network.train(training_sequence2, 1)
 
-    error_same.append(network.compute_raining_error(test_sequence1))
+    error_same.append(network.compute_error(test_sequence1))
     error_diff.append(network.compute_error(test_sequence2))
+    error_rand.append(network.compute_error(test_sequence3))
 
-plt.plot(rounds, error_same, label="Same sequence")
-plt.plot(rounds, error_diff, label="different sequence")
+plt.plot(rounds, error_same, label="abc")
+plt.plot(rounds, error_diff, label="bac")
+plt.plot(rounds, error_rand, label="only b's")
 plt.legend(loc=2)
 plt.xlabel("number of training rounds")
 plt.ylabel("cross-entropy")
