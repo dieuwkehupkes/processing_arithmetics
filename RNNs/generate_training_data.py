0f07663534f475c30dc5532ff3c8b62de1d6a639
dieuwkehupkes <dieuwkehupkes@gmail.com>
2016-04-21 17:24:03 +0200

    Add function to generate input-output pairs for architecture 1

--- RNNs/generate_training_data.py
+++ RNNs/generate_training_data.py
@@ -6,7 +6,7 @@ from arithmetics import mathTreebank
 from arithmetics import mathExpression
 import re
 
-def generate_treebank(language, architecture=1):
+def generate_treebank(languages, architecture):
     """
     Generate training examples.
     :param language_dict:   dictionary mapping languages to number of samples
@@ -16,16 +16,34 @@ def generate_treebank(language, architecture=1):
         lengths, operators, branching = parse_language(name)
         treebank.addExamples(operators, branching=branching, lengths=lengths, n=N) 
 
-    training_data = generate_training_data(treebank)
-    return training_data
+    return treebank
 
-def generate_training_data(treebank):
+def generate_training_data(languages, architecture):
     """
     Take a treebank object and return numpy
     arrays with training data.
     """
-    treebank.write_to_file('treebank')
-    return treebank
+    # generate treebank with examples
+    treebank = generate_treebank(languages, architecture)
+
+    # create map from digits and operators to integers
+    digits = [treebank.digits]
+    operators = [treebank.operators]
+    digits.sort()
+    N = len(digits) + len(operators) + 2
+    d_map = zip(digits+treebank + ['(',')'], np.arange(1, N+1))
+
+    # create empty input and targets
+    X, Y = [], []
+
+    # loop over examples
+    for expression, answer in treebank.examples:
+        input_seq = [d_map[i] for i in str(expression).split()]
+        answer = d_map[str(answer)]
+        X.append(input_seq)
+        Y.append(answer)
+
+    return X, Y
 
 def parse_language(language_str):
     """
