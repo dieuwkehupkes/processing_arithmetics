665b591464bf11c7e88b07123cfb62270e88b0fd
dieuwkehupkes <dieuwkehupkes@gmail.com>
2016-04-21 15:47:17 +0200

    Create file to generate training data
    generate_training_data uses class MathTreebank to generate
    training data.
    Currently the file contains a function to parse language names
    such that examples with the right features can be generated, and
    a function that generates a treebank object with the required number
    of example sentences.
    I should still write a function that converts this into numpy arrays,
    based on information about the initial word embeddings.

--- RNNs/generate_training_data.py
+++ RNNs/generate_training_data.py
@@ -1,9 +1,59 @@
 """
-Functions to generate trainng data for the
+Functions to generate training data for the
 arithmetic task.
 """
+from arithmetics import mathTreebank
+from arithmetics import mathExpression
+import re
 
+def generate_treebank(language, architecture=1):
+    """
+    Generate training examples.
+    :param language_dict:   dictionary mapping languages to number of samples
+    """
+    treebank = mathTreebank()
+    for name, N in languages.items():
+        lengths, operators, branching = parse_language(name)
+        treebank.addExamples(operators, branching=branching, lengths=lengths, n=N) 
 
-def generate_training_data():
-    # write this function
-    return
+    training_data = generate_training_data(treebank)
+    return training_data
+
+def generate_training_data(treebank):
+    """
+    Take a treebank object and return numpy
+    arrays with training data.
+    """
+    treebank.write_to_file('treebank')
+    return treebank
+
+def parse_language(language_str):
+    """
+    Give in a string for a language, return
+    a tuple with arguments to generate examples.
+    :return:    (#leaves, operators, branching)
+    """
+    # find # leaves
+    nr = re.compile('[0-9]+')
+    n = int(nr.search(language_str).group())
+
+    # find operators
+    plusmin = re.compile('\+')
+    op = plusmin.search(language_str)
+    if op:
+        operators = [op.group()]
+    else:
+        operators = ['+','-']
+
+    # find branchingness
+    branch = re.compile('left|right')
+    branching = branch.search(language_str)
+    if branching:
+        branching = branching.group()
+
+    return ([n], operators, branching)
+
+if __name__ == '__main__':
+    languages = {'L_3lb-':10, 'L_4+':5}
+    generate_treebank(languages)
+
