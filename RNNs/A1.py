07d385b4ee65f97e16612a9153437dfefdc120a5
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2016-04-23 21:30:08 +0200

    Remove unused code and inputs

--- RNNs/A1.py
+++ RNNs/A1.py
@@ -1,5 +1,5 @@
 # imports
-from keras.models import Graph, Model
+from keras.models import Model
 from keras.layers import SimpleRNN, Embedding, Dense, GRU, LSTM, Input
 from TrainingHistory import TrainingHistory
 from auxiliary_functions import generate_embeddings_matrix
@@ -47,15 +47,6 @@ comparison = Dense(size_compare, name='comparison')(recurrent)               # c
 output_layer = Dense(1, activation=output_activation, name='output')(comparison)
 
 model = Model(input=input_layer,  output=output_layer)
-
-
-# Create model
-# model = Graph()
-# model.add_input(name='input', input_shape=(1,), dtype='int')        # input layer
-# model.add_node(Embedding(input_dim=input_dim, output_dim=input_size, input_length=input_length, weights=W_embeddings, mask_zero=mask_zero, trainable=embeddings), name='embeddings', input='input')      # embeddings layer
-# model.add_node(recurrent_layer(size_hidden), name='recurrent_layer', input='embeddings')    # recurrent layer
-# model.add_node(Dense(size_compare), name='comparison', input='recurrent_layer')         # comparison layer
-# model.add_node(Dense(1, activation=output_activation), name='output', input='comparison', create_output=True)
 
 # compile model
 model.compile(loss={'output':'mean_squared_error'}, metrics=['accuracy'], optimizer=optimizer)
