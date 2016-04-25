c40741527442f48a39e12ac9c26b2a382fdd87af
dieuwkehupkes <dieuwkehupkes@gmail.com>
2016-04-25 12:38:43 +0200

    Add parameter to fix comparison layer during training

--- RNNs/simulations.py
+++ RNNs/simulations.py
@@ -10,14 +10,15 @@ size_hidden         = 20            # size of the hidden layer
 size_compare        = 10            # size of comparison layer
 
 # INPUT
-cotrain             = True          # set to true for cotraining of embeddings
+cotrain_embeddings  = True          # set to true for cotraining of embeddings
+cotrain_comparison  = True          # set to true for cotraining of comparison layer
 encoding            = 'gray'        # options: random, gray
 mask_zero           = True          # set to true to apply masking to input
 input_size          = 6             # input dimensionality
 # Compute input dimension and input length from training data
 
 # TRAINING
-nb_epoch            = 1500          # number of iterations
+nb_epoch            = 100          # number of iterations
 batch_size          = 24            # batchsize during training
 validation_split    = 0.1          # fraction of data to use for testing
 verbose             = 1             # verbosity mode
@@ -26,11 +27,9 @@ optimizer           = 'adagrad'     # sgd, rmsprop, adagrad, adadelta, adam, ada
 # generate for that language. 
 # languages \in L_i, L_i+, L_i-, L_iright, L_ileft for 1<i<8)
 # languages           = {'L_2':5, 'L_3':5}            # dict L -> N
-languages           = {'L_2':1000}                    # dict L -> N
+languages           = {'L_2':2000}                    # dict L -> N
 
-history = architecture(languages=languages, input_size=input_size, size_hidden=size_hidden, size_compare=size_compare, recurrent=recurrent_layer, encoding=encoding, trainable=cotrain, mask_zero=mask_zero, optimizer=optimizer, validation_split=validation_split, batch_size=batch_size, nb_epoch=nb_epoch, verbose=verbose)
-
-# raw_input()
+history = architecture(languages=languages, input_size=input_size, size_hidden=size_hidden, size_compare=size_compare, recurrent=recurrent_layer, encoding=encoding, trainable_embeddings=cotrain_embeddings, trainable_comparison=cotrain_comparison, mask_zero=mask_zero, optimizer=optimizer, validation_split=validation_split, batch_size=batch_size, nb_epoch=nb_epoch, verbose=verbose)
 
 # plot results
 plt.plot(history.losses, label='loss training set')
