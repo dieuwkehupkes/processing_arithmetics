da53f34e1c57732886d3a51b5ddd0378714917ef
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2016-04-25 11:20:24 +0200

    change on_batch_end to on_epoch_end

--- RNNs/TrainingHistory.py
+++ RNNs/TrainingHistory.py
@@ -3,11 +3,15 @@ from keras.callbacks import Callback
 class TrainingHistory(Callback):
     def on_train_begin(self, logs={}):
         self.losses = []
+        self.val_losses = []
         self.accuracy = []
+        self.val_accuracy = []
         self.i = 0
     
-    def on_batch_end(self, batch, logs={}):
+    def on_epoch_end(self, batch, logs={}):
         self.losses.append(logs.get('loss'))
+        self.val_losses.append(logs.get('val_loss'))
         self.accuracy.append(logs.get('acc'))
+        self.val_accuracy.append(logs.get('val_acc'))
         self.i += 1
         
