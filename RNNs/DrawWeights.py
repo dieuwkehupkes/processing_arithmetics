5bacd3b6df69ec3e13614ba255404a56a2050b8b
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2016-04-29 12:08:35 +0200

    Try to fix error

--- RNNs/DrawWeights.py
+++ RNNs/DrawWeights.py
@@ -6,7 +6,6 @@ import matplotlib.animation as animation
 class DrawWeights(Callback):
 
     def __init__(self, figsize, layer_id=1, param_id=0, weight_slice=(slice(None), 0)):
-        # TODO find a different way to access weight layers, this gives an error
         self.layer_id = layer_id
         self.param_id = param_id
         self.weight_slice = weight_slice
@@ -15,15 +14,24 @@ class DrawWeights(Callback):
         self.ax = self.fig.add_subplot(1, 1, 1)
 
     def on_train_begin(self, logs={}):
+        # check if weights of layer_id are accessible
+        try:
+            self.model.layers[self.layer_id].get_weights()[self.param_id]
+        except IndexError:
+            print "Weights of layer %s cannot be visualised" % self.model.layers[self.layer_id].name
         self.imgs = []
 
-    def on_batch_end(self, batch, logs={}):
-        # Get a snapshot of the weight matrix every 5 batches
+    def on_epoch_end(self, batch, logs={}):
+        # Get a snapshot of the weight matrix every 5 epochs
         if batch % 5 == 0:
             # Access the full weight matrix
-            weights = self.model.layers[self.layer_id].params[self.param_id].get_value()
+            weights = self.model.layers[self.layer_id].get_weights()[self.param_id]
             # Create the frame and add it to the animation
+            print weights
+            # print weights[self.weight_slice].shape
             img = self.ax.imshow(weights[self.weight_slice], interpolation='nearest')
+            # img = self.ax.imshow(weights, interpolation='nearest')
+            plt.plot()
             self.imgs.append(img)
 
     def on_train_end(self, logs={}):
