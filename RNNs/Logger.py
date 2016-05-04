18dc0fa76d3c26c5f3eca66f0ed0b3b18f1934a3
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2016-05-04 12:55:13 +0200

    Add callback function that prints intermediate results at requested times

---
+++ RNNs/Logger.py
@@ -0,0 +1,28 @@
+from keras.callbacks import Callback
+
+
+# noinspection PyAttributeOutsideInit
+class Logger(Callback):
+
+    def __init__(self, N):
+        """
+        Print summary of current losses and accuracy
+        every N epochs
+        """
+        self.N = N
+
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.val_losses = []
+        self.prediction_error = []
+        self.val_prediction_error = []
+        self.i = 0
+    
+    def on_epoch_end(self, epoch, logs={}):
+        if epoch %  self.N:
+            # give summary of current results
+            print('Epoch%i' % epoch)
+            print('training loss: %f - val loss: %f - training mspe: %f - val mspe: %f'
+                  % (logs.get('loss'), logs.get('val_loss'),
+                     logs.get('mean_squared_prediction_error'),
+                     logs.get('val_mean_squared_prediction_error')))
