940af022db447285a4da6664424d89d218009289
dieuwkehupkes <dieuwkehupkes@gmail.com>
2016-05-09 16:33:58 +0200

    add print function for settings file

--- RNNs/auxiliary_functions.py
+++ RNNs/auxiliary_functions.py
@@ -4,9 +4,25 @@ Collection of auxiliary functions.
 Describe functions?
 """
 
-
 import numpy as np
-import theano.tensor as T
+# import theano.tensor as T
+
+
+# function to print summary of results
+def print_sum(settings):
+    print('Model summary:')
+    print('Recurrent layer: %s' % str(settings.recurrent_layer))
+    print('Size hidden layer: %i' % settings.size_hidden)
+    print('Size comparison layer: %i' % settings.size_compare)
+    print('Initialisation embeddings: %s' % settings.encoding)
+    print('Size embeddings: %i' % settings.input_size)
+    print('Batch size: %i' % settings.batch_size)
+    print('Number of epochs: %i' % settings.nb_epoch)
+    print('Optimizer: %s' % settings.optimizer)
+    print('Trained on:')
+    for language, nr in settings.languages_train.items():
+        print('%i sentences from %s' % (nr, language))
+
 
 # define padding function
 def pad(array, length):
