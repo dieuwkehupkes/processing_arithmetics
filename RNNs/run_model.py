cef572f6c11089ccd7821a159270ec3750f86800
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2016-06-22 15:21:56 +0200

    Adapt run_model for adapted GRU layer\n\nI should still add plotting functions for gate values

--- RNNs/run_model.py
+++ RNNs/run_model.py
@@ -1,6 +1,7 @@
 from keras.models import Model, model_from_json
 from keras.layers import Embedding, Input, GRU, LSTM, SimpleRNN, Dense
 from analyser import visualise_hidden_layer
+from GRU_output_gates import GRU_output_gates
 from architectures import A1
 from architectures import *
 import argparse
@@ -27,6 +28,7 @@ model.load_weights(settings.model_weights)
 model.compile(optimizer=settings.optimizer, loss=settings.loss, metrics=settings.metrics)
 
 dmap = pickle.load(open(settings.model_dmap, 'rb'))
+dmap['x'] = 0
 dmap_inverted = dict([(item[1],item[0]) for item in dmap.items()])
 id = settings.architecture.get_recurrent_layer_id()
 maxlen = model.layers[id].input_shape[1]
@@ -57,7 +59,7 @@ else:
 # GENERATE MODEL TRUNCATED AT RECURRENT LAYER
 
 # check embeddings layer type:
-layer_type = {'SimpleRNN': SimpleRNN, 'GRU': GRU, 'LSTM': LSTM}
+layer_type = {'SimpleRNN': SimpleRNN, 'GRU': GRU_output_gates, 'LSTM': LSTM}
 recurrent_layer = layer_type[model.get_config()['layers'][id]['class_name']]
 rec_config = model.layers[id].get_config()
 
@@ -79,7 +81,7 @@ if settings.compute_correls:
     # loop over test items
     for name, X_test, Y_test in test_data:
         predictions = truncated_model.predict(X_test)
-        non_zero = predictions[np.any(predictions!=0, axis=2)]
+        non_zero = predictions[np.any(predictions!=0, axis=1)]
         print np.corrcoef(non_zero)
 
 
@@ -88,6 +90,8 @@ if settings.compute_correls:
 if settings.project_lexical:
     output_dim = truncated_model.layers[-1].output_dim
     lex_size = len(dmap)
+    if 0 in dmap.values():
+        lex_size -= 1
     hl_activations = np.zeros(output_dim*lex_size).reshape(lex_size, output_dim)
     i = 0
     labels = []
@@ -100,20 +104,23 @@ if settings.project_lexical:
             return token
 
     for lex_item in sorted(dmap.keys(), key=conv):
+        if dmap[lex_item] == 0:
+            continue
         input_seq = np.array([[dmap[lex_item]]])
         seq_padded = keras.preprocessing.sequence.pad_sequences(input_seq, dtype='int32', maxlen=maxlen)
-        hl_activation = truncated_model.predict(seq_padded)
+        hl_activation = truncated_model.predict(seq_padded)[:,:,:output_dim]
         hl_non_zero = hl_activation[np.any(hl_activation!=0, axis=2)]
         hl_activations[i] = hl_non_zero
         labels.append(lex_item)
         i+=1
 
-    visualise_hidden_layer(hl_activations, labels)
+    visualise_hidden_layer([hl_activations, labels])
 
 
 ###########################################################################################
 # VISUALISE TEST ITEMS ONE BY ONE
 if settings.one_by_one:
+    output_dim = truncated_model.layers[-1].output_dim
     user_input = None
     i = 0
     hl_activations = []
@@ -122,7 +129,10 @@ if settings.one_by_one:
             predictions = model.predict(X_test)
             model_predictions = predictions.round()
         for s, m in zip(X_test, Y_test):
-            labels = [dmap_inverted[word] for word in s[s.nonzero()]]
+            # labels = [dmap_inverted[word] for word in s[s.nonzero()]]
+            # find first non zero
+            nz1 = s.nonzero()[0][0]
+            labels = [dmap_inverted[word] for word in s[nz1:]]
             test_item = ' '.join(labels)
             correct_prediction = str(m)
             if settings.architecture == A1:
@@ -131,7 +141,10 @@ if settings.one_by_one:
                 model_prediction = "No scalar prediction for A4 architecture"
             print("Test item: %s\t\t Correct prediction: %s\t\t Model prediction: %s"
                   % (test_item, correct_prediction, model_prediction))
-            hl_activation = truncated_model.predict(np.array([s]))
+            outputs = truncated_model.predict(np.array([s]))
+            hl_activation = outputs[:,:,:output_dim]
+            z = outputs[:,:,output_dim:2*output_dim]
+            r = outputs[:,:,2*output_dim:]
             new_input = (hl_activation, labels)
             hl_activations.append((new_input))
             i += 1
