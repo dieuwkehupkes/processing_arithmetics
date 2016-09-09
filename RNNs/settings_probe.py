6b2a9e9383ff22fed25faaca7f65ff5b75cd5d2c
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2016-09-09 12:53:18 +0200

    new settings

--- RNNs/settings_probe.py
+++ RNNs/settings_probe.py
@@ -6,22 +6,24 @@ model_architecture = pref+'.json'
 model_weights = pref+'_weights.h5'
 dmap = pref+'.dmap'
 
-classifiers         = ['grammatical']
-nb_epoch            = 3000
+classifiers         = ['grammatical', 'intermediate_locally']
+# classifiers         = ['intermediate_locally']
+# classifiers         = ['grammatical']
+nb_epochs           = 5000
 
 optimizer           = 'adam'
 dropout_recurrent   = 0.0
 batch_size          = 24
-nb_epochs           = 3
 verbosity           = 2
 
 validation_split    = 0.1
 
 digits              = np.arange(-10,11)
 
-languages_train             = {'L1':1, 'L2': 2, 'L4':1, 'L6':1}
 # languages_train             = {'L1':3000, 'L2': 3000, 'L4':3000, 'L6':3000}
+languages_train             = {'L1':3, 'L2': 2, 'L4':1, 'L6':1}
 languages_val               = None
 languages_test              = {'L3': 400, 'L5':400, 'L7':400}
+test_separately             = True
 maxlen                      = max_length(15)
 
