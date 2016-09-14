0186cd4c10f121127b7742673153fca0f2852a48
dieuwkehupkes <dieuwkehupkes@gmail.com>
2016-09-14 16:11:03 +0200

    Add classifiers

--- seqbased/settings_probe.py
+++ seqbased/settings_probe.py
@@ -6,22 +6,21 @@ model_architecture = pref+'.json'
 model_weights = pref+'_weights.h5'
 dmap = 'models/dmap'
 
-classifiers         = ['grammatical', 'intermediate_locally']
-# classifiers         = ['intermediate_locally']
-# classifiers         = ['grammatical']
+classifiers         = ['grammatical', 'intermediate_locally', 'intermediate_recursively', 'subtracting', 'top_stack']
+# classifiers         = ['top_stack']
 nb_epochs           = 10
 
 optimizer           = 'adam'
 dropout_recurrent   = 0.0
 batch_size          = 24
-verbosity           = 2
+verbosity           = 1
 
 validation_split    = 0.1
 
 digits              = np.arange(-10,11)
 
 # languages_train             = {'L1':3000, 'L2': 3000, 'L4':3000, 'L6':3000}
-languages_train             = {'L1':3, 'L2': 2, 'L4':1, 'L6':1}
+languages_train             = {'L3':3, 'L2': 2, 'L4':1, 'L6':1}
 languages_val               = None
 # languages_val               = {'L3': 400, 'L5':400, 'L7':400}
 languages_test              = {'L3': 400, 'L5':400, 'L7':400}
