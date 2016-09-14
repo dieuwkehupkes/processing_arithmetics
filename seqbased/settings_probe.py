fb2e998008d3651e3606a24512ab3a2a8a9a79d5
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2016-09-14 09:59:05 +0200

    Make general dmap and add classifier param to test file

--- seqbased/settings_probe.py
+++ seqbased/settings_probe.py
@@ -4,7 +4,7 @@ from auxiliary_functions import max_length
 pref = 'models/GRU_A1_2'
 model_architecture = pref+'.json'
 model_weights = pref+'_weights.h5'
-dmap = pref+'.dmap'
+dmap = 'models/dmap'
 
 classifiers         = ['grammatical', 'intermediate_locally']
 # classifiers         = ['intermediate_locally']
@@ -20,8 +20,8 @@ validation_split    = 0.1
 
 digits              = np.arange(-10,11)
 
-languages_train             = {'L1':3000, 'L2': 3000, 'L4':3000, 'L6':3000}
-# languages_train             = {'L1':3, 'L2': 2, 'L4':1, 'L6':1}
+# languages_train             = {'L1':3000, 'L2': 3000, 'L4':3000, 'L6':3000}
+languages_train             = {'L1':3, 'L2': 2, 'L4':1, 'L6':1}
 languages_val               = None
 # languages_val               = {'L3': 400, 'L5':400, 'L7':400}
 languages_test              = {'L3': 400, 'L5':400, 'L7':400}
