579149ccd70e32ad6ab2b0e67dc86e155545d400
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2016-09-14 09:37:22 +0200

    Fix plots
    The not plotting was due to using the wrong backend for matplotlib. This
    is a little bit tricky because on the server the backend that is needed
    to plot seems to cause an error with Tkinder and once the backend for
    matplotlib is set it cannot be changed anymore it seems, which means
    that in order for this file to work also the backend in the files
    containing the imported classes should be changed (even though the
    imports weren't directly imported). I should find a solution for this.

--- seqbased/make_accuracy_plots.py
+++ seqbased/make_accuracy_plots.py
@@ -1,20 +1,22 @@
 import sys
+sys.path.insert(0, '../commonFiles') 
 from test_model import test_model
 from architectures import A1, A4
 from collections import OrderedDict
 import pickle
 import numpy as np
-import matplotlib.pyplot as plt
-sys.path.insert(0, '../commonFiles') 
 from arithmetics import mathTreebank
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
-prefs = 'models/GRU_A1_1', 'models/GRU_A1_2'
+prefs = 'models/GRU_A1_1', 'models/GRU_A1_2', 'models/GRU_A4_A1_2-2'
 architecture = A1
 N = 5000
-languages = OrderedDict(zip(['L1','L2','L3'],3*[N]))
+languages = OrderedDict(zip(['L1','L2','L3','L4','L5','L6','L7','L8','L9'], 9*[N]))
 metrics = ['mean_squared_prediction_error', 'binary_accuracy']
 digits = np.arange(-10,11)
-maxlen=57
+maxlen = 57
 
 # generate test data
 treebanks = []
@@ -31,50 +33,45 @@ for name, treebank in treebanks:
     for expression, answer in treebank.examples:
         outcome = expression.solveAlmost()
         se.append(np.square(outcome - answer))
-        correct.append(outcome==answer)
+        correct.append(outcome == answer)
 
     # add outcome to dictionary
     strategy_results['mean_squared_prediction_error'].append(np.mean(se))
     strategy_results['binary_accuracy'].append(np.mean(correct))
 
 # compute mspe and binary accuracy for models
-for pref in prefs:
-    model_architecture = pref+'.json'
-    model_weights = pref+'_weights.h5'
-    model_dmap = pref+'.dmap' 
-
+for metric in metrics:
     results_all = dict()
+    for pref in prefs:
+        model_architecture = pref+'.json'
+        model_weights = pref+'_weights.h5'
+        model_dmap = pref+'.dmap' 
 
-    results = test_model(architecture=architecture, model_architecture=model_architecture, model_weights=model_weights, dmap=model_dmap, optimizer='adam', metrics=metrics, loss='mse', digits=digits, test_sets=treebanks)
+        results = test_model(architecture=architecture, model_architecture=model_architecture, model_weights=model_weights, dmap=model_dmap, optimizer='adam', metrics=metrics, loss='mse', digits=digits, test_sets=treebanks)
 
-    results_all[pref] = results
+        results_all[pref] = results
 
-    # generate plots
-    for metric in metrics:
-        x = np.arange(3)
-        y_max = 3*[1]
+    # plot symbolic approaches
+    x = np.arange(1,10)
+    if metric == 'mean_squared_prediction_error':
+        y_max = 9*[0]
+    elif metric == 'binary_accuracy':
+        y_max = 9*[1]
 
-        # plot symbolic approaches
-        plt.plot(x, strategy_results[metric], label='SolveAlmost')
-        plt.plot(x, y_max, label='SolveRecursive')
-        plt.plot(x, y_max, label='SolveIncremental')
+    plt.plot(x, strategy_results[metric], label='SolveAlmost')
+    plt.plot(x, y_max, label='SolveRecursive')
+    plt.plot(x, y_max, label='SolveIncremental')
 
-        # plot different models
-        for model in  results_all:
-            results = results_all[model][metric].values()
-            plt.plot(x, results, label=model)
+    # plot different models
+    for model in results_all:
+        results = results_all[model][metric].values()
+        plt.plot(x, results, label=model)
 
-        plt.legend()
+    plt.title(metric)
+    plt.xlabel("number of numeric elements")
+    plt.ylabel(metric)
+    plt.legend()
 
-plt.show()
-
-
-
-
-
-
-
-
-
+    plt.show()
 
 
