451a473ee33547a13a31fbe7e713a1bad24653ce
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2016-09-26 09:43:04 +0200

    make probe_model run with new param sample weights

--- seqbased/probe_model.py
+++ seqbased/probe_model.py
@@ -9,14 +9,14 @@ import argparse
 import re
 import pickle
 from auxiliary_functions import generate_embeddings_matrix, print_sum, save_model
-from architectures import Probing, A1
+from architectures import Probing, A1, Training
 
 
-def train_model(architecture, weights, dmap, classifiers, digits, languages_train, optimizer, dropout_recurrent, batch_size, nb_epochs, validation_split, languages_val, verbosity, maxlen):
+def train_model(architecture, weights, dmap, classifiers, digits, languages_train, optimizer, dropout_recurrent, batch_size, nb_epochs, validation_split, sample_weights, languages_val, verbosity, maxlen):
 
-    training = Probing(classifiers)
+    training = Probing()
 
-    training.add_pretrained_model(architecture, weights, dmap, copy_weights=['recurrent', 'embeddings'], train_classifier=True, train_embeddings=False, train_recurrent=False, mask_zero=True, dropout_recurrent=dropout_recurrent, optimizer=optimizer)
+    training.add_pretrained_model(architecture, weights, dmap, copy_weights=['recurrent', 'embeddings'], train_classifier=True, train_embeddings=False, train_recurrent=False, mask_zero=True, classifiers=classifiers, dropout_recurrent=dropout_recurrent, optimizer=optimizer)
 
     dmap = pickle.load(open(dmap, 'rb'))
 
@@ -28,11 +28,11 @@ def train_model(architecture, weights, dmap, classifiers, digits, languages_trai
         validation_data = (X_val, Y_val)
 
 
-    training.train((X_train, Y_train), batch_size=batch_size, epochs=nb_epochs, validation_split=validation_split, validation_data=validation_data, verbosity=verbosity)
+    training.train((X_train, Y_train), batch_size=batch_size, epochs=nb_epochs, validation_split=validation_split, validation_data=validation_data, sample_weight=sample_weights, verbosity=verbosity)
 
     return training
 
-def test_model(training, languages_test, dmap, digits, maxlen, test_separately, classifiers):
+def test_model(training, languages_test, dmap, digits, maxlen, test_separately, classifiers, sample_weights):
     """
     Test model on new test set, plot also the training and validation
     error the model had during training
@@ -44,7 +44,7 @@ def test_model(training, languages_test, dmap, digits, maxlen, test_separately, 
     dmap = pickle.load(open(dmap, 'rb'))
 
     # generate test data
-    test_data = training.generate_test_data(settings.languages_test, dmap=dmap,
+    test_data = Training.generate_test_data(Probing, settings.languages_test, dmap=dmap,
                                             digits=digits,
                                             pad_to=maxlen,
                                             classifiers=classifiers,
@@ -70,7 +70,7 @@ def test_model(training, languages_test, dmap, digits, maxlen, test_separately, 
 
         # '\t'.join(['%s: %f' % (item[0], item[1][-1]) for item in hist.metrics_val.items()])
     for name, X, Y in test_data:
-        acc = training.model.evaluate(X, Y)
+        acc = training.model.evaluate(X, Y, sample_weight=sample_weights)
         print "\nAccuracy for for test set %s:" % name,
         print '\t'.join(['%s: %f' % (training.model.metrics_names[i], acc[i]) for i in xrange(len(acc))])
 
@@ -90,10 +90,10 @@ if __name__ == '__main__':
 
     settings = __import__(import_string)
 
-    training = train_model(architecture=settings.model_architecture, weights=settings.model_weights, dmap=settings.dmap, digits=settings.digits, languages_train=settings.languages_train, classifiers=settings.classifiers, optimizer=settings.optimizer, dropout_recurrent=settings.dropout_recurrent, batch_size=settings.batch_size, maxlen=settings.maxlen, nb_epochs=settings.nb_epochs, validation_split=settings.validation_split, languages_val=settings.languages_val, verbosity=settings.verbosity)
+    training = train_model(architecture=settings.model_architecture, weights=settings.model_weights, dmap=settings.dmap, digits=settings.digits, languages_train=settings.languages_train, classifiers=settings.classifiers, optimizer=settings.optimizer, dropout_recurrent=settings.dropout_recurrent, batch_size=settings.batch_size, maxlen=settings.maxlen, nb_epochs=settings.nb_epochs, validation_split=settings.validation_split, languages_val=settings.languages_val, sample_weights=settings.sample_weights, verbosity=settings.verbosity)
 
     if settings.languages_test:
-        test_model(training, settings.languages_test, settings.dmap, settings.digits, settings.maxlen, settings.test_separately, settings.classifiers)
+        test_model(training, settings.languages_test, settings.dmap, settings.digits, settings.maxlen, settings.test_separately, settings.classifiers, sample_weights=settings.sample_weights)
 
     # save model
     save_model(training)
--- seqbased/settings_probe.py
+++ seqbased/settings_probe.py
@@ -6,14 +6,14 @@ model_architecture = pref+'.json'
 model_weights = pref+'_weights.h5'
 dmap = 'models/dmap'
 
-classifiers         = ['grammatical', 'intermediate_locally', 'intermediate_recursively', 'subtracting', 'top_stack']
+classifiers         = ['grammatical', 'intermediate_locally', 'intermediate_recursively', 'subtracting']
 # classifiers         = ['top_stack']
-nb_epochs           = 10
+nb_epochs           = 1
 
 optimizer           = 'adam'
 dropout_recurrent   = 0.0
 batch_size          = 24
-verbosity           = 1
+verbosity           = 2
 
 validation_split    = 0.1
 
@@ -24,6 +24,7 @@ languages_train             = {'L1':3000, 'L2': 3000, 'L4':3000, 'L6':3000}
 languages_val               = None
 # languages_val               = {'L3': 400, 'L5':400, 'L7':400}
 languages_test              = {'L3': 500, 'L5':500, 'L7':500, 'L7_left':500}
+sample_weights              = None
 test_separately             = True
 maxlen                      = max_length(15)
 
