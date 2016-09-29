7630801f816803b93425baa7001df900ecd4d5c4
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2016-09-29 16:40:31 +0200

    automatically create filename based on parameters

--- seqbased/settings_train.py
+++ seqbased/settings_train.py
@@ -6,7 +6,8 @@ from arithmetics import training_treebank, test_treebank, heldout_treebank
 import numpy as np
 
 # numpy seed
-seed = 0
+seed = 7
+save_every = 500                 
 
 # network details
 architecture        = A1            # Trainings architecture
@@ -22,6 +23,9 @@ encoding            = 'random'        # options: random, gray
 mask_zero           = True          # set to true to apply masking to input
 input_size          = 2             # input dimensionality
 
+n = {GRU: 'GRU', SimpleRNN: 'SRN'}
+filename = 'models_run2/'+n[recurrent_layer]+str(size_hidden)+ '_seed' + str(seed) + '_'
+
 # PRETRAIN
 # Use this to train an already trained model
 pretrained_model = None
@@ -30,7 +34,7 @@ pretrained_model = None
 copy_weights = None
 
 # TRAINING
-nb_epoch            = 2            # number of iterations
+nb_epoch            = 1500            # number of iterations
 batch_size          = 24            # batchsize during training
 validation_split    = 0.1          # fraction of data to use for testing
 optimizer           = 'adam'     # sgd, rmsprop, adagrad, adadelta, adam, adamax
@@ -40,8 +44,7 @@ sample_weights      = None
 # generate for that language.
 # languages \in L_i, L_i+, L_i-, L_iright, L_ileft for 1<i<8)
 digits                      = np.arange(-10, 11)
-# languages_train             = training_treebank(seed=seed)
-languages_train             = {'L3': 6}
+languages_train             = training_treebank(seed=seed)
 format                      = 'infix'
 languages_val              = heldout_treebank(seed=seed)
 languages_test              = [(name, treebank) for name, treebank in test_treebank(seed=seed)]
@@ -52,6 +55,4 @@ maxlen                      = max_length(15)
 weights_animation = False           # create an animation of layer weights,
                                     # provide tuple of layer and param_id
 verbose = 2                         # verbosity mode
-save_every = 1                 # print results
-filename = 'test'
 
