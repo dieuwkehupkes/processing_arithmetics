15abb6a41b4fcdee8e52d53cd07fad3a53cd303c
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2016-12-31 15:51:06 +0100

    Add subclass of keras.Model that has dmap as attribute
    To make sure that models will not be ran with the wrong dmap, create a
    new class that is a subclass of the keras.Model class that has dmap set
    as an attribute.

--- processing_arithmetics/Tests/Tests.py
+++ processing_arithmetics/Tests/Tests.py
@@ -1,19 +1,19 @@
 import pytest
 import numpy as np
 from processing_arithmetics.arithmetics import MathTreebank
-from processing_arithmetics.seqbased.architectures import A1, A4, Probing, Seq2Seq, Training
+from processing_arithmetics.seqbased.architectures import ScalarPrediction, ComparisonTraining, DiagnosticClassifier, Seq2Seq, Training
 from keras.layers import GRU
 import pickle
 import os
 
-def test_A1_methods():
-    _test_architecture_methods(A1)
+def test_ScalarPrediction_methods():
+    _test_architecture_methods(ScalarPrediction)
 
-def test_A4_methods():
-    _test_architecture_methods(A4)
+def test_ComparisonTraining_methods():
+    _test_architecture_methods(ComparisonTraining)
 
-def test_Probing_methods():
-    _test_architecture_methods(Probing, extra_classifiers=['subtracting'])
+def test_DiagnosticClassifier_methods():
+    _test_architecture_methods(DiagnosticClassifier, extra_classifiers=['subtracting'])
 
 def test_Seq2Seq_methods():
     _test_architecture_methods(Seq2Seq)
@@ -23,13 +23,15 @@ def _test_architecture_methods(architecture, extra_classifiers=None):
     Test if methods of Training class still work.
     """
 
+    digits = np.arange(-10, 11)
+    operators = ['+', '-']
+
     # generate architecture, load dmap
-    A = architecture()
-    dmap = pickle.load(open('seqbased/best_models/dmap','r'))
+    A = architecture(digits=digits, operators=operators)
 
     # test build model
-    A.generate_model(recurrent_layer=GRU, input_dim=len(dmap)+1, input_length=40,
-                     input_size=2, size_hidden=3, dmap=dmap,
+    A.generate_model(recurrent_layer=GRU, input_length=40,
+                     input_size=2, size_hidden=3,
                      extra_classifiers=extra_classifiers)
 
     # test save model
@@ -37,28 +39,30 @@ def _test_architecture_methods(architecture, extra_classifiers=None):
     os.remove('saved_model')
 
     # test add pretrained model
-    A.add_pretrained_model(A.model, dmap=dmap, classifiers=extra_classifiers)
+    A.add_pretrained_model(A.model, classifiers=extra_classifiers)
 
     # test save model
     A.save_model('saved_model')
     os.remove('saved_model')
 
     # test generate training data from dictionary
-    training_data = Training.generate_training_data(architecture, {'L1':5, 'L3l':10, 'L4r':10}, dmap=dmap, pad_to=40, classifiers=extra_classifiers)
+    training_data = A.generate_training_data({'L1':5, 'L3l':10, 'L4r':10}, pad_to=40, classifiers=extra_classifiers)
 
     # test generate training data from math treebank?
     m = MathTreebank({'L1':5, 'L3l':10, 'L4r':10}, digits=np.arange(-10, 11))
-    validation_data = Training.generate_training_data(architecture, m, dmap=dmap, pad_to=40, classifiers=extra_classifiers)
-    # TODO implement this
+    validation_data = A.generate_training_data(m, pad_to=40, classifiers=extra_classifiers)
 
     # test train
+    if os.path.exists('temp1.h5'):
+        os.remove('temp1.h5')
     A.train(training_data, batch_size=2, epochs=1, filename='temp', validation_data=validation_data)
 
     os.remove('temp1.h5')
 
+    # TODO use actually the Training.test for this
     # test generate test data
     languages = {'L1':10, 'L2':15, 'L3':20}
-    test_data = Training.generate_test_data(A, data=languages, dmap=dmap, digits=np.arange(-10, 11), pad_to=40, classifiers=extra_classifiers, test_separately=True)
+    test_data = A.generate_test_data(data=languages, digits=np.arange(-10, 11), pad_to=40, classifiers=extra_classifiers, test_separately=True)
 
     # test model testing
     for name, X, Y in test_data:
