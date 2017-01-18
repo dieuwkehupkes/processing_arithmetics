5d5cb7b1d63cc188f1f922637e1592a951c98e0f
dieuwkehupkes <dieuwkehupkes@gmail.com>
2017-01-18 17:45:25 +0100

    Add fixture to arithmetics test

--- tests/test_arithmetics.py
+++ tests/test_arithmetics.py
@@ -2,17 +2,21 @@ import pytest
 import numpy as np
 from processing_arithmetics.arithmetics.MathTreebank import MathTreebank
 
-def test_solve_locally_infix():
-    _test_solve_locally('infix')
+@pytest.fixture(params=[
+    'infix', 'prefix'
+])
+def format(request):
+    return request.param
 
-def test_solve_locally_prefix():
-    _test_solve_locally('prefix')
 
-def test_solve_recursively_infix():
-    _test_solve_recursively('infix')
 
-def test_solve_recursively_prefix():
-    _test_solve_recursively('prefix')
+def test_solve_locally(format):
+    _test_solve_locally(format)
+
+
+def test_solve_recursively(format):
+    _test_solve_recursively(format)
+
 
 def test_solve_recursively_postfix():
     _test_solve_recursively('postfix')
