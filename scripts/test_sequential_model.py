d8decc1bb6c1d05ab8e114846d9ab72640f6306d
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2017-04-04 10:32:54 +0200

    Add classifiers to seq script

--- scripts/test_sequential_model.py
+++ scripts/test_sequential_model.py
@@ -21,6 +21,7 @@ parser = argparse.ArgumentParser()
 # positional arguments
 parser.add_argument("architecture", type=get_architecture, help="Type of architecture used during training: scalar prediction, comparison training, seq2seq or a diagnostic classifier", choices=[ScalarPrediction, ComparisonTraining, DiagnosticClassifier, Seq2Seq])
 parser.add_argument("model", type=str, help="Filename with extension h5 containing a keras model")
+parser.add_argument("-classifiers", required=True, nargs="*", choices=['subtracting', 'intermediate_locally', 'intermediate_recursively', 'grammatical', 'intermediate_directly', 'depth', 'minus1depth', 'minus2depth', 'minus3depth', 'minus4depth'])
 parser.add_argument("--format", type=str, help="Set formatting of arithmetic expressions", choices=['infix', 'postfix', 'prefix'], default="infix")
 
 parser.add_argument("--seed_test", type=int, help="Set random seed for testset", default=100)
@@ -29,11 +30,11 @@ parser.add_argument("metrics", nargs='*', help="Add if you want to test metrics 
 
 args = parser.parse_args()
 
-languages_test = [(name, treebank) for name, treebank in treebank(seed=args.seed_test, kind='test')]
+languages_test = [(name, tb) for name, tb in treebank(seed=args.seed_test, kind='test')]
 digits = np.arange(-10, 11)
 operators = ['+', '-']
 
-architecture = args.architecture(digits=digits, operators=operators)
+architecture = args.architecture(digits=digits, operators=operators, classifiers=args.cassifiers)
 architecture.add_pretrained_model(model=args.model)
 test_data = architecture.generate_test_data(data=languages_test, digits=digits)
 architecture.test(test_data, metrics=args.metrics)  # TODO test if this also works for multilpe outputs
